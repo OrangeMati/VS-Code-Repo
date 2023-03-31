34e12408c519b9f009d6bffe7917569e1961baa5
OrangeMati <127123068+OrangeMati@users.noreply.github.com>
2023-03-31 11:08:18 +0200

    Update Pythonscript.py

--- Pythonscript.py
+++ Pythonscript.py
@@ -1,4 +1,4 @@
 import calendar
-year =int( input("Enter the year of the required calendar "))
-month = int( input("Enter the month of the required calendar "))
+year = int( input("Enter the year of the required calendar in the format YYYY"))
+month = int( input("Enter the month of the required calendar in the format MM"))
 print(calendar.month(year,month))
